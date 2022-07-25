b58c3218a3e6fd7b6566d131050316b75d1d5c33
Marcelo Lares <marcelo.lares@gmail.com>
2022-07-25 11:52:27 -0300

    compare

--- histograms/tools.py
+++ histograms/tools.py
@@ -1,6 +1,67 @@
 import numpy as np
 
-def cdffit(x, n, ns):
+def cdffit(x, n, ns=100, use_data=False):
+    """
+    x: data
+    n: number of Fourier components
+    np: number of points to sample the model
+    use_data: if True, return the model evaluated at data points
+    """
+    x.sort()
+    y = np.linspace(0, 1, len(x))
+    ref = (x-x.min())/(x.max()-x.min())
+    res = y - ref
+    cols = []
+    f = np.pi / (max(x)-min(x))
+    for i in range(1, n+1):
+        cols.append(np.sin(f*i*(x-min(x))))
+    M = np.column_stack(cols)
+    A = M.transpose()
+    pars = np.linalg.solve(A@M, A@res)
+    
+    if use_data:
+        model = np.repeat(0, len(x))
+        for i, a in enumerate(pars):
+            mit = a*np.sin(f*(i+1)*(x-min(x)))
+            model = model + mit
+        ref = (x-x.min())/(x.max()-x.min())
+        return x, model + ref
+    else:
+        t = np.linspace(min(x), max(x), ns)
+        model = np.repeat(0, ns)
+        for i, a in enumerate(pars):
+            mit = a*np.sin(f*(i+1)*(t-min(t)))
+            model = model + mit        
+        ref = (t-x.min())/(x.max()-x.min())
+        return t, model + ref
+
+    
+def cdffit_func(x, n, t):
+    """
+    x: data
+    n: number of Fourier components
+    t: scalar, a value to evaluate the fitted CDF
+    """
+    x.sort()
+    y = np.linspace(0, 1, len(x))
+    ref = (x-x.min())/(x.max()-x.min())
+    res = y - ref
+    cols = []
+    f = np.pi / (max(x)-min(x))
+    for i in range(1, n+1):
+        cols.append(np.sin(f*i*(x-min(x))))
+    M = np.column_stack(cols)
+    A = M.transpose()
+    pars = np.linalg.solve(A@M, A@res)
+    
+    model = 0
+    for i, a in enumerate(pars):
+        mit = a*np.sin(f*(i+1)*(t-min(x)))
+        model = model + mit
+    ref = (t-x.min())/(x.max()-x.min())
+    return model + ref
+
+def cdffit_modelonly(x, n, ns):
     """
     x: data
     n: number of Fourier components
@@ -23,7 +84,6 @@ def cdffit(x, n, ns):
         mit = a*np.sin(f*(i+1)*(t-min(t)))
         model = model + mit
     return model
-
 
 def pdffit_num(x, n, ns):
     """
@@ -67,8 +127,6 @@ def pdffit(x, n, ns):
     n: number of Fourier components
     np: number of points to sample the model
     """
-    n = 7
-
     x.sort()
     y = np.linspace(0, 1, len(x))
     ref = (x-x.min())/(x.max()-x.min())
@@ -88,4 +146,6 @@ def pdffit(x, n, ns):
     for i, a in enumerate(pars):
         mit = a*np.cos(f*(i+1)*(t-min(t)))*f*(i+1)
         modelpdf = modelpdf + mit
+        
+    modelpdf = np.maximum(0, modelpdf)
     return t, modelpdf
